abdb218fc7768f5c5ebe521e7925a2a4440eeb30
TungVy <deadice08@gmail.com>
2023-06-11 01:22:28 +0700

    Week1.Bai1. tao cac thuoc tinh, phuong thuc get, set, va khoi tao constructor

--- Coordinates.java
+++ Coordinates.java
@@ -1,4 +1,44 @@
 package Week1;
 
 public class Coordinates {
+
+//  cac thuoc tinh: toa do x, toa do y va ten toa do.
+    private int x;
+    private int y;
+    private String nameCoor;
+//  khoi tao mot constructor mac dinh.
+    public Coordinates() {
+    }
+
+//  khoi tao mot constructor day du tham so.
+    public Coordinates(int x, int y, String nameCoor) {
+        this.x = x;
+        this.y = y;
+        this.nameCoor = nameCoor;
+    }
+
+//  phuong thuc thiet lap get, set
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public String getNameCoor() {
+        return nameCoor;
+    }
+
+    public void setNameCoor(String nameCoor) {
+        this.nameCoor = nameCoor;
+    }
 }
