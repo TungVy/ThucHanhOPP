8f5edc1cc5a630d37c6748ecc7aa4969ad31e7bb
TungVy <deadice08@gmail.com>
2023-06-11 02:02:53 +0700

    Week1.Bai2. tao bien, constructor, getter, setter

--- Student.java
+++ Student.java
@@ -1,4 +1,55 @@
 package Week1;
 
 public class Student {
+
+//    khoi tao bien
+    private int id;
+    private String fullName;
+    private float theoryMark;
+    private float practiceMark;
+
+//    Constructor mac dinh
+    public Student() {
+    }
+
+    //    Conatructor day du tham so
+    public Student(int id, String fullName, float theoryMark, float practiceMark) {
+        this.id = id;
+        this.fullName = fullName;
+        this.theoryMark = theoryMark;
+        this.practiceMark = practiceMark;
+    }
+
+//    getter, setter
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public float getTheoryMark() {
+        return theoryMark;
+    }
+
+    public void setTheoryMark(float theoryMark) {
+        this.theoryMark = theoryMark;
+    }
+
+    public float getPracticeMark() {
+        return practiceMark;
+    }
+
+    public void setPracticeMark(float practiceMark) {
+        this.practiceMark = practiceMark;
+    }
 }
