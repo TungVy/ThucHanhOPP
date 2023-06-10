aee5c4ad9c182d70fe9faa0d45c83512a1bb1782
TungVy <deadice08@gmail.com>
2023-06-11 01:41:13 +0700

    Week1. xay dung ham main kiem nghiem bai 1

--- Main.java
+++ Main.java
@@ -1,4 +1,13 @@
 package Week1;
 
 public class Main {
+    public static void main(String args[]) {
+//        kiem nghiem bai 1:
+        Coordinates a = new Coordinates(5, 6, "A");
+        Coordinates b = new Coordinates(1, 8, "B");
+
+        System.out.println(a);
+        System.out.println(b);
+
+    }
 }
