a418bf6dbc5fb7fac040a367dced4000ab2d7713
TungVy <deadice08@gmail.com>
2023-06-21 11:41:27 +0700

    Week1.Bai2. chinh sua kiem tra rang buoc du lieu va gan gia tri mac dinh khong hop le

--- Student.java
+++ Student.java
@@ -14,10 +14,39 @@ public class Student {
 
     //    Conatructor day du tham so
     public Student(int id, String fullName, float theoryMark, float practiceMark) {
-        this.id = id;
-        this.fullName = fullName;
-        this.theoryMark = theoryMark;
-        this.practiceMark = practiceMark;
+        if (id < 0){
+            this.id = 0;
+            System.out.println("Wrong!!");
+        }
+        else{
+            this.id = id;
+        }
+        if (fullName == "") {
+            this.fullName = "Wrong!!";
+        }
+        else {
+            this.fullName = fullName;
+        }
+        if (theoryMark > 10) {
+            this.theoryMark = 10;
+        }
+        else if (theoryMark < 0) {
+            this.theoryMark = 0;
+        }
+        else {
+            this.theoryMark = theoryMark;
+        }
+        if (practiceMark > 10) {
+            this.practiceMark = 10;
+        }
+        else if (practiceMark < 0) {
+            this.practiceMark = 0;
+        }
+        else {
+            this.practiceMark = practiceMark;
+        }
+
+        average();
     }
 //      tinh diem trung binh
     public float average() {
@@ -30,44 +59,42 @@ public class Student {
         return id+" "+fullName+" "+theoryMark+" "+practiceMark+" "+average();
     }
 
-    //    getter, setter
+    public void printStudent() {
+        System.out.printf("%-5s%-25s%-15.1f%-15.1f%-15.1f \n", id, fullName, theoryMark, practiceMark, average());
+    }
+
+
+//    getter, setter
     public int getId() {
-        if (id < 0)
-            return 1;
         return id;
     }
+
     public void setId(int id) {
         this.id = id;
     }
 
     public String getFullName() {
-        if (fullName == "")
-            return "Wrong!!";
         return fullName;
     }
+
     public void setFullName(String fullName) {
         this.fullName = fullName;
     }
 
     public float getTheoryMark() {
-        if (theoryMark >10)
-            return 10;
-        else if (theoryMark < 0)
-            return 0;
-        return 0f;
+        return theoryMark;
     }
+
     public void setTheoryMark(float theoryMark) {
         this.theoryMark = theoryMark;
     }
 
     public float getPracticeMark() {
-        if (practiceMark >10)
-            return 10;
-        else if (practiceMark < 0)
-            return 0;
-        return 0f;
+        return practiceMark;
     }
+
     public void setPracticeMark(float practiceMark) {
         this.practiceMark = practiceMark;
     }
 }
+
