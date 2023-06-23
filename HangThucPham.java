7f543a5732392ee959586a1cb6bcb3426ab888ee
TungVy <deadice08@gmail.com>
2023-06-23 22:18:05 +0700

    Week1.Bai5. khoi tao bien, constructor, getter, setter

--- HangThucPham.java
+++ HangThucPham.java
@@ -1,5 +1,67 @@
 package Week1;
 
+import java.time.LocalDate;
+
 public class HangThucPham {
-    
+
+//    khoi tao bien
+    private String maHang;
+    private String tenHang;
+    private double donGia;
+    private LocalDate ngaySanXuat;
+    private LocalDate ngayHetHan;
+
+//    constructor mac dinh
+    public HangThucPham() {
+    }
+
+//    constructor day du tham so
+    public HangThucPham(String maHang, String tenHang, double donGia, LocalDate ngaySanXuat, LocalDate ngayHetHan) {
+        this.maHang = maHang;
+        this.tenHang = tenHang;
+        this.donGia = donGia;
+        this.ngaySanXuat = ngaySanXuat;
+        this.ngayHetHan = ngayHetHan;
+    }
+
+//    getter, setter
+    public String getMaHang() {
+        return maHang;
+    }
+
+    public void setMaHang(String maHang) {
+        this.maHang = maHang;
+    }
+
+    public String getTenHang() {
+        return tenHang;
+    }
+
+    public void setTenHang(String tenHang) {
+        this.tenHang = tenHang;
+    }
+
+    public double getDonGia() {
+        return donGia;
+    }
+
+    public void setDonGia(double donGia) {
+        this.donGia = donGia;
+    }
+
+    public LocalDate getNgaySanXuat() {
+        return ngaySanXuat;
+    }
+
+    public void setNgaySanXuat(LocalDate ngaySanXuat) {
+        this.ngaySanXuat = ngaySanXuat;
+    }
+
+    public LocalDate getNgayHetHan() {
+        return ngayHetHan;
+    }
+
+    public void setNgayHetHan(LocalDate ngayHetHan) {
+        this.ngayHetHan = ngayHetHan;
+    }
 }
